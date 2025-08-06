2e44cb251b725b63e1f44af65a30124b8d9e34e6
anourine <anourine@innovqube.com>
2025-08-06 14:33:38 +0200

    fix input's font-size in smartphone display

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -19,7 +19,7 @@ function SearchOrder() {
         placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className="focus:ring-opacity-50 focus:ring-yellow-500w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-700 sm:focus:w-72 focus:ring focus:outline-none w-35 sm:w-64"
+        className="focus:ring-opacity-50 focus:ring-yellow-500w-28 rounded-full bg-yellow-100 px-4 py-2 text-[18px] transition-all duration-300 placeholder:text-stone-700 sm:focus:w-72 focus:ring focus:outline-none w-46.5 sm:w-64"
       />
     </form>
   );
